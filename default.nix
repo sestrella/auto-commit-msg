c2bdf03ce25754cd3cb7d4d5783fe2bf6e279ab9
Sebastian Estrella <2049686+sestrella@users.noreply.github.com>
2025-10-21 21:07:13 -0500

    refactor(default.nix): rename binary to acm
    Renames the built binary from `auto-commit-msg` to `acm` for brevity. Updates `mainProgram` in `meta` accordingly.
    
    auto-commit-msg{Version:0.3.0-dev Model:gemini-2.5-flash-lite ResponseTime:1.440573375s ExecutionTime:1.447713667s}

--- default.nix
+++ default.nix
@@ -18,11 +18,13 @@ pkgs.buildGoApplication {
       ./version.txt
     ];
   };
-  # TODO: change the name of the binary
   modules = ./gomod2nix.toml;
+  postInstall = ''
+    mv $out/bin/auto-commit-msg $out/bin/acm
+  '';
   meta = {
     description = "Generates a commit message from a git diff using AI";
     license = lib.licenses.mit;
-    mainProgram = "auto-commit-msg";
+    mainProgram = "acm";
   };
 }
